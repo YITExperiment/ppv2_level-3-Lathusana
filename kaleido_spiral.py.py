31677d92118877fbedc99870f58260c539c13905
Lathusana <134749247+Lathusana@users.noreply.github.com>
2023-06-01 11:18:02 +0530

    Add files via upload

--- kaleido_spiral.py.py
+++ kaleido_spiral.py.py
@@ -1,4 +1,11 @@
-import turtle               
+import turtle
+
+def draw_circle(size):
+    turtle.pencolor('Red')
+    turtle.circle(size)
+    draw_circle(size)
+
+turtle               
 from itertools import cycle
 colors = cycle(['red', 'orange', 'yellow', 'green', 'blue', 'purple'])
 
@@ -9,9 +16,9 @@ def draw_circle(size,angle,shift):
     turtle.circle(size)
     turtle.right(angle)
     turtle.forward(shift)
-    draw_circle(size+0, angle+0,shift+0)
+    draw_circle(size+0.5, angle+1,shift+1)
     
 turtle.bgcolor('black')
 turtle.speed('fastest')
 turtle.pensize(5)
-draw_circle(40,5,5)
+draw_circle(30,0,1)
